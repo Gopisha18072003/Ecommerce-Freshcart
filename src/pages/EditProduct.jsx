608c49ec81437ee6bc6316bb692f1929c55faf5d
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2024-08-14 21:27:59 +0000

    stylling of edit products finished

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -149,7 +149,6 @@ export default function EditProduct() {
               </div>
             </div>
           )}
-          <div className="flex gap-[4rem] justify-center items-start">
           <div className="w-[12rem] mx-auto">
             <img
               src={product.image}
@@ -291,7 +290,6 @@ export default function EditProduct() {
               </button>
             </div>
           </form>
-          </div>
           <div className=" flex justify-center py-8">
           <button
             className="p-3 bg-red-500 text-white poppins-semibold rounded-md text-lg"
