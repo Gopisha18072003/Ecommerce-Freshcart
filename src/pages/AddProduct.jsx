3cfa496221633912994b0ccf629deb8fe9a2b3f4
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2024-08-14 21:18:42 +0000

    styling addProduct

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -55,6 +55,7 @@ export default function AddProduct() {
   return (
     <div className="mt-[12rem]">
         <h1 className="poppins-bold text-2xl my-8 text-center">Add Product</h1>
+        <div className="flex gap-[4rem] items-center justify-center">
         <div className="w-[12rem] mx-auto ">
           <img
             src={imagePreview}
@@ -216,6 +217,8 @@ export default function AddProduct() {
           </button>
         </div>
       </form>
+        </div>
+        
       <div className="h-[8rem]">
 
       </div>
